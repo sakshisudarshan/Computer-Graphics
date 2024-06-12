c9c1da46964fd0b8cd084c3b942e92d819508fb2
sakshisudarshan <144573279+sakshisudarshan@users.noreply.github.com>
2024-06-12 18:20:47 +0530

    Create DisplayList1Obj.cpp
    PROGRAM TO IMPLEMENT DISPLAY LIST

---
+++ DisplayList1Obj.cpp
@@ -0,0 +1,38 @@
+#include<GL/glut.h>
+#include<math.h>
+
+void myinit() {
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(-1, 1, -1, 1);
+	glMatrixMode(GL_MODELVIEW);
+}
+
+void display() {
+	glClearColor(1, 1, 1, 0);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(0, 0, 0);
+	glNewList(1, GL_COMPILE);
+	glBegin(GL_POLYGON);
+	glVertex2f(-0.5, -0.5);
+	glVertex2f(-0.5, 0.5);
+	glVertex2f(0.5, 0.5);
+	glVertex2f(0.5, -0.5);
+	glEnd();
+	glEndList();
+	glCallList(1);
+	glFlush();
+}
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowPosition(50, 50);
+	glutInitWindowSize(500, 500);
+	glutCreateWindow("Display Lists");
+	myinit();
+	glutDisplayFunc(display);
+	glutMainLoop();
+    return 0;
+}
