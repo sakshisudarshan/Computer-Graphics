519e2959743860b8d4fc2e0719bc9fa7b9b7aa2c
sakshisudarshan <144573279+sakshisudarshan@users.noreply.github.com>
2024-08-04 23:07:56 +0530

    Create BresenhamsLineDrawingAlgorithm.cpp
    PROGRAM TO DRAW A LINE USING BRESENHAM’S LINE DRAWING TECHNIQUE

---
+++ BresenhamsAlgorithm.cpp
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <GL/glut.h>
+#include <math.h>
+
+void myinit()
+{
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0,500,0,500);
+	glMatrixMode(GL_MODELVIEW);
+}
+
+void setPixel(int x, int y) {
+    glBegin(GL_POINTS);
+    glVertex2d(x, y);
+    glEnd();
+}
+
+void lineBres(int x0, int y0, int xEnd, int yEnd) {
+    int dx = fabs(xEnd - x0), dy = fabs(yEnd - y0);
+    int p = 2 * dy - dx;
+    int twoDy = 2 * dy, twoDyMinusDx = 2 * (dy - dx);
+    int x, y;
+
+    if (x0 > xEnd) {
+        x = xEnd;
+        y = yEnd;
+        xEnd = x0;
+    } else {
+        x = x0;
+        y = y0;
+    }
+
+    setPixel(x, y);
+    while (x < xEnd) {
+        x++;
+        if (p < 0)
+            p += twoDy;
+        else {
+            y++;
+            p += twoDyMinusDx;
+        }
+        setPixel(x, y);
+    }
+}
+
+void display() {
+    glClearColor(0, 0, 0, 0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1.0, 1.0, 1.0);
+
+    int x0 = 50, y0 = 100;
+    int xEnd = 200, yEnd = 300;
+
+    lineBres(x0, y0, xEnd, yEnd);
+
+    glFlush();
+}
+
+int main(int argc, char **argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(500, 500);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Bresenham Line Drawing");
+    myinit();
+    glutDisplayFunc(display);
+    glutMainLoop();
+    return 0;
+}
